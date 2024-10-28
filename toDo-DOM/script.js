e3cf589081b1afa7ef72fa137a605ccf5b63a0f9
SG0704179 <Affan.Zaidi.ctr@sabre.com>
2024-10-28 14:01:29 +0530

    Initial commit.

---
+++ toDo-DOM/script.js
@@ -0,0 +1,38 @@
+const body = document.body;
+
+const form = document.createElement("form");
+form.className = "main-form";
+
+const input = document.createElement("input");
+input.placeholder = "Enter an item to add";
+input.className = "input";
+
+const button = document.createElement("button");
+button.innerText = "Add Item";
+
+const ul = document.createElement("ul");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  var toDoItem = input.value.trim();
+  if (!toDoItem) {
+    alert("Please enter an item before adding!");
+  } else {
+    const li = document.createElement("li");
+    li.innerText = toDoItem;
+    ul.appendChild(li);
+  }
+  li.onclick = () => {
+    li.remove();
+  };
+  input.value = "";
+};
+
+form.append(input, button);
+body.append(form, ul);
+
+// ucreate this in react without jsx
+
+// using createRoot and createElement
+
+//  fo remove padding in the footnotes tab feature/FOotnotes-core
